839afc18c25575d485c1b49243b68ab1b88bd6bf
powerkessler <powerkessler@gmail.com>
2017-06-06 12:55:56 -0400

    i added init methods

--- Run.java
+++ Run.java
@@ -1,8 +1,27 @@
 public class run {
+  
+  KeyInput keyInput;
+  Window gameWindow;
+  ArrayList<Character> characters;
+  ArrayList<Mob> mobs;
+  
   public static void main(String[] args0){
-    new RUn();
+    new Run();
   }
+  
   public Run(){
-   //do something 
+  characters = new ArrayList<Character>();
+  mobs = new ArrayList<Mob>();
+  initKeyInput();
+  initWindow();
   }
+  
+  void initKeyInput(){
+    
+  }
+  
+  void initWindow(){
+    
+  }
+  
 }
